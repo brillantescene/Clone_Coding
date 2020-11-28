9b424b37b41a510b3ac2f08265904014f2013cc8
yujin <glisteneugene@gmail.com>
2020-11-28 23:23:45 +0900

    table

---
+++ AppleMusic/AppleMusic/Sources/Cells/Table/LATableCell4.swift
@@ -0,0 +1,24 @@
+//
+//  LATableCell4.swift
+//  AppleMusic
+//
+//  Created by ✨EUGENE✨ on 2020/11/28.
+//
+
+import UIKit
+
+class LATableCell4: UITableViewCell {
+
+    static let identifier = "LATableCell4"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
