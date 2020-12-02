1acbdbfb501cac6d3a27d73861d8097d8f5b60ec
yujin <glisteneugene@gmail.com>
2020-12-02 23:31:07 +0900

    table도 nib 시작

---
+++ AppleMusic/AppleMusic/Sources/Cells/Table/LATVCell1.swift
@@ -0,0 +1,27 @@
+//
+//  LATVCell1.swift
+//  AppleMusic
+//
+//  Created by ✨EUGENE✨ on 2020/12/02.
+//
+
+import UIKit
+
+class LATVCell1: UITableViewCell {
+
+    static let identifier = "LATVCell1"
+    @IBOutlet var collectionView: UICollectionView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
