4d3c23d04093b42fdbe6a7985f8af51c450a5488
yujin <glisteneugene@gmail.com>
2020-12-15 22:56:23 +0900

    당근마켓 다시

---
+++ CarrotMarket/CarrotMarket/Sources/Cells/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  CarrotMarket
+//
+//  Created by ✨EUGENE✨ on 2020/12/15.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
